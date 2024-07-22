3e4035d3f6bcf1b56bd1047f308732bcd1539fb0
Sourabh Takshak <takshaksourabh11@gmail.com>
2024-07-23 02:25:10 +0530

    ✨ Added components & services

---
+++ src/app/components/add-workout/add-workout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-workout',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-workout.component.html',
+  styleUrl: './add-workout.component.scss'
+})
+export class AddWorkoutComponent {
+
+}
