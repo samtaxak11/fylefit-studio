4d6a593abd7162bc91aa4b979994ffd4a458d6ff
Sourabh Takshak <takshaksourabh11@gmail.com>
2024-07-24 16:58:04 +0530

    ✨Added routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ExploreComponent } from "./components/explore/explore.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ExploreComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'fylefit-studio';
